a6ccf87d8fe74fb2700a59decd1fe653b34a6997
Chih-Che Chang <et00026@gmail.com>
2015-10-21 14:41:59 +0800

    Add check_db_nodata.sh

--- script/check_db_nodata.sh
+++ script/check_db_nodata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Title:    Nagios Plugin to check names which no data over 3 days in DB.
+# Title:    Nagios Plugin to check name which no data over 3 days in DB.
 # History:
 # Date,         Time,   Author, Version,    Description.
 # 2015/10/16,   19:33,  CCC,    v1,         Created Plugin.
@@ -13,15 +13,15 @@ USERNAME="USERNAME"
 IP=10.10.10.10
 STARTDATE=$(date --date='3 days ago' +%Y-%m-%d)
 ENDDATE=$(date +%Y-%m-%d)
-USERNAME=$(sudo psql -d $DBNAME -U $USERNAME -h $IP -w -c \
+ACCOUNT=$(sudo psql -d $DBNAME -U $USERNAME -h $IP -w -c \
     "COPY (SELECT username FROM accounts) TO STDOUT WITH CSV")
 
-# Variable of names
-list_nodata_names=""
-num_nodata_names=0
+# Variable of name
+list_nodata_name=""
+num_nodata_name=0
 
-# Find out no data over 3 day names in DB
-for name in ${USERNAME[@]}; do
+# Find out no data over 3 day name in DB
+for name in ${ACCOUNT[@]}; do
     exits=$(sudo psql -d $DBNAME -U $USERNAME -h $IP -w -c \
     "select exists ( \
         SELECT * FROM table WHERE tstamp >= '$STARTDATE' \
@@ -29,26 +29,26 @@ for name in ${USERNAME[@]}; do
         AND username = '$name');" |grep -E '(t|f)')
 
     if [ $exits  = "f" ]; then
-        list_nodata_names+="$name "
-        ((num_nodata_names+=1))
+        list_nodata_name+="$name "
+        ((num_nodata_name+=1))
     fi
 done
 
 # Nagios status
-if [ $num_nodata_names -lt 10 ]; then
-    echo "OK- $num_nodata_names over 3 days no data.|$list_nodata_names"
+if [ $num_nodata_name -lt 10 ]; then
+    echo "OK- $num_nodata_name over 3 days no data.|$list_nodata_name"
     exit 0
 
-elif [ $num_nodata_names -gt 10 ] && [ $num_nodata_names -lt 25 ]; then
-    echo "WARNING- $num_nodata_names over 3 days no data.|$list_nodata_names"
+elif [ $num_nodata_name -gt 10 ] && [ $num_nodata_name -lt 25 ]; then
+    echo "WARNING- $num_nodata_name over 3 days no data.|$list_nodata_name"
     exit 1
 
-elif [ $num_nodata_names -gt 25 ]; then
-    echo "CRITICAL- $num_nodata_names over 3 days no data.|$list_nodata_names"
+elif [ $num_nodata_name -gt 25 ]; then
+    echo "CRITICAL- $num_nodata_name over 3 days no data.|$list_nodata_name"
     exit 2
 
 else
-    echo "UNKNOWN- Something wrong.|num_nodata_names: $num_nodata_names"
+    echo "UNKNOWN- Something wrong.|num_nodata_name: $num_nodata_name"
     exit 3
 
 fi
