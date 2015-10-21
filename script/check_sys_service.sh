c6397b6965458b37ff05c453eebe57bec7b307fa
colorbook <et00026@gmail.com>
2015-10-21 15:13:14 +0800

    Add review note in check_sys_service.sh

--- script/check_sys_service.sh
+++ script/check_sys_service.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Title:    Nagios Plugin to check nagios client snort service.
+# History:
+# Date,         Time,   Author, Version,    Description.
+# 2015/10/16,   20:30,  CCC,    v1,         Created Plugin.
+# REFERENCE:
+#   https://assets.nagios.com/downloads/nagioscore/docs/nagioscore/3/en/pluginapi.html
+
+#!/bin/bash
+
+# Get host ip and setting check snort service(defaults  number of snort service is 2),
+# and supervisor automatically run snort service.
 host_ip=$1
 num_check_srv=$(sudo ssh root@$host_ip "supervisorctl status|grep snort|grep RUNNING|wc -l")
 
@@ -9,7 +20,7 @@ if sudo ssh root@$host_ip "ps ax|grep -v grep|grep supervisord >> /dev/null";the
     if [ $num_check_srv == 2 ];then
         echo "OK- snort service is OK!"
         exit 0
-
+        
     elif [ $num_check_srv == 1 ];then
         echo "WARNING- snort service is not all running!|Restart snort service."
         sudo ssh root@$host_ip "supervisorctl reload"
